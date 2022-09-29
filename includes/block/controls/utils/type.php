9a5ac8d5de4d377ce8afd6001d3c33890e94eb09
Nicolas Jaussaud <nicolas@teamtangible.com>
2022-09-29 17:38:02 -0400

    Beaver builder: Fix object to array conversion

--- includes/block/controls/utils/type.php
+++ includes/block/controls/utils/type.php
@@ -5,7 +5,7 @@ defined('ABSPATH') or die();
 /**
  * @see https://stackoverflow.com/a/4345578/10491705
  */
-$plugin->object_to_array = function($data) {
+$plugin->object_to_array = function($data) use($plugin) {
 
   if( ! is_array($data) && ! is_object($data) ) return $data;
 
@@ -13,7 +13,7 @@ $plugin->object_to_array = function($data) {
 
   foreach( $data as $key => $value ) {
     $result[$key] = (is_array($value) || is_object($value)) 
-      ? object_to_array($value) 
+      ? $plugin->object_to_array($value) 
       : $value
     ;
   }
