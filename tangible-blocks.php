daa0117c62776362d3e619130551d484349a27f6
eliot-akira <me@eliotakira.com>
2023-05-27 14:20:06 +0200

    3.2.5

--- tangible-blocks.php
+++ tangible-blocks.php
@@ -3,13 +3,13 @@
  * Plugin Name: Tangible Blocks
  * Plugin URI: https://tangibleblocks.com/
  * Description: Tangible Blocks is a system for universal blocks that work in Gutenberg, Elementor, and Beaver Builder
- * Version: 3.2.4
+ * Version: 3.2.5
  * Author: Team Tangible
  * Author URI: https://teamtangible.com
  * License: GPLv2 or later
  */
 
-define( 'TANGIBLE_BLOCKS_VERSION', '3.2.4' );
+define( 'TANGIBLE_BLOCKS_VERSION', '3.2.5' );
 
 require_once __DIR__ . '/vendor/tangible/plugin-framework/index.php';
 require_once __DIR__ . '/vendor/tangible/plugin-updater/index.php';
